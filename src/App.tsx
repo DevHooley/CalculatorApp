72068091ecce574dd73b568401f5562c08594d90
Your Name <92824573+kyleehooley@users.noreply.github.com>
2023-04-07 09:45:16 -0500

    Starter

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-xl w-32 mx-auto capitalize">
+        hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
